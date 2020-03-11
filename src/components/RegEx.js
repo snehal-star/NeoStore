a94c7da553c3700dfdbaf31c2b655e4cc3e941f9
snehalpatil <snehalpro55@gmail.com>
2020-03-11 17:51:50 +0530

    added RegEx.js

---
+++ src/components/RegEx.js
@@ -0,0 +1,7 @@
+export const RegEx ={
+    FirstName:/[A-Za-z]/,
+    LastName:/[A-Za-z]/,
+    Email:/^([a-zA-Z_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,
+    Password:/[A-za-z]/,
+    MobileNo:/[0-9]/
+}
